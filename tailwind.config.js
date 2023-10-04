ce31a21726999938c5bc3d8e4bb385e235de845f
sejahterastore <sejahterastore0@gmail.com>
2023-10-05 06:02:10 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './public/index.html',
+    ],
+    theme: {
+        extend: {
+            letterSpacing: {
+                nanas: '.5em',
+                ngempit: '.-0.5em',
+            }
+        },
+    },
+    plugins: [],
+}
